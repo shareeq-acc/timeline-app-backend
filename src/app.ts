eefa27f78eb02ebf386afe56133bab8ea87d6cfa
shareeq-acc <xmartshareeq2@gmail.com>
2026-06-19 16:20:50 +0500

    fix(config): cors setting changed

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 // Middleware
 app.use(express.json()); 
 app.use(cors({
-  // origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: process.env.CLIENT_URL || 'http://localhost:3000',
   credentials: true // Allow credentials (cookies)
 })); 
 app.use(cookieParser());
